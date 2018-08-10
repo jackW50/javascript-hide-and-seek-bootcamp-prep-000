d315f3c200f90b55204a7903222cf534758fe522
John Werenski <jweredc@gmail.com>
2018-08-10 16:02:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget(id) {
-  let list1 = document.getElementById(id);
-  return list1.querySelectorAll('div.target');
+function nestedTarget() {
+  return querySelector('#nested.target');
 }
 
 function increaseRankBy(n) {
-  let list2 = document.querySelectorAll('ul.ranked-list li');
+  let list2 = document.querySelectorAll('.ranked-list');
   for (let i = 0; i < list2.length; i++) {
     list2[i].innerHTML = (i + parseInt(n));
   }
